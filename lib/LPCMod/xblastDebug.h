d297aac8307b7cbdc18d1dd7d9c10e9f681339c1
cromwelldev <cromwelldev>
2018-04-24 15:51:24 -0400

    Added DBG_LVL_OFF level to completely shut off debug log for a module
    Added default Debug log levels for modules.

--- lib/LPCMod/xblastDebug.h
+++ lib/LPCMod/xblastDebug.h
@@ -17,35 +17,37 @@
 #define DBG_LVL_INFO      2
 #define DBG_LVL_DEBUG     1
 #define DBG_LVL_TRACE     0   /* Not implement for now */
+#define DBG_LVL_OFF DBG_LVL_FATAL + 1
+
 
 #define CURRENT_DBG_LVL DBG_LVL_WARN
 
 #ifdef DEBUGLOGGER
-#define DEBUG_ALWAYS_SHOW       1
-#define DEBUG_BOOT_LOG          0
-#define DEBUG_FLASH_DRIVER      0
-#define DEBUG_FLASH_LOWLEVEL    0
-#define DEBUG_FLASH_UI          0
-#define DEBUG_EXCEPTIONS        0
-#define DEBUG_IDE_DRIVER        0   /* Not printed in text log to avoid logger loops */
-#define DEBUG_IDE_LOCK          0
-#define DEBUG_VIDEO_DRIVER      0
-#define DEBUG_EEPROM_DRIVER     0
-#define DEBUG_LWIP              1
-#define DEBUG_HW_ID             0
-#define DEBUG_GENERAL_UI        0
-#define DEBUG_SCRIPT            0
-#define DEBUG_MISC              0
-#define DEBUG_SETTINGS          0
+#define DEBUG_ALWAYS_SHOW       DBG_LVL_INFO
+#define DEBUG_BOOT_LOG          DBG_LVL_INFO
+#define DEBUG_FLASH_DRIVER      DBG_LVL_FATAL
+#define DEBUG_FLASH_LOWLEVEL    DBG_LVL_FATAL
+#define DEBUG_FLASH_UI          DBG_LVL_INFO
+#define DEBUG_EXCEPTIONS        DBG_LVL_DEBUG
+#define DEBUG_IDE_DRIVER        DBG_LVL_INFO   /* Not printed in text log to avoid logger loops */
+#define DEBUG_IDE_LOCK          DBG_LVL_INFO
+#define DEBUG_VIDEO_DRIVER      DBG_LVL_WARN
+#define DEBUG_EEPROM_DRIVER     DBG_LVL_INFO
+#define DEBUG_LWIP              DBG_LVL_INFO
+#define DEBUG_HW_ID             DBG_LVL_INFO
+#define DEBUG_GENERAL_UI        DBG_LVL_WARN
+#define DEBUG_SCRIPT            DBG_LVL_INFO
+#define DEBUG_MISC              DBG_LVL_INFO
+#define DEBUG_SETTINGS          DBG_LVL_INFO
 
-#define DEBUG_USB               0
-#define DEBUG_USB_INFO          0
-#define DEBUG_USB_ERR           0
-#define DEBUG_USB_WARN          0
+#define DEBUG_USB               0   //TODO: fix
+#define DEBUG_USB_INFO          0   //TODO: fix
+#define DEBUG_USB_ERR           0   //TODO: fix
+#define DEBUG_USB_WARN          0   //TODO: fix
 
-#define DEBUG_FATX_FS           0
-#define DEBUG_CORE_FATFS        0   /* Not printed in text log to avoid logger loops */
-#define DEBUG_LOGGER            0   /* Will essentially double the string prints on SPI. Not printed in text log to avoid logger loops */
+#define DEBUG_FATX_FS           DBG_LVL_DEBUG
+#define DEBUG_CORE_FATFS        DBG_LVL_WARN    /* Not printed in text log to avoid logger loops */
+#define DEBUG_LOGGER            DBG_LVL_ERROR   /* Will essentially double the string prints on SPI. Not printed in text log to avoid logger loops */
 
 #if DEBUG_USB >= 1
 #define DEBUG 1
